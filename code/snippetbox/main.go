cf656452e4be383e0d3d4881b90d24ea05f25b21
TB <80381086+tbuiG@users.noreply.github.com>
2024-03-19 00:37:09 +0000

    http status code

--- code/snippetbox/main.go
+++ code/snippetbox/main.go
@@ -6,6 +6,14 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't, use
+	// the http.NotFound() function to send a 404 response to the client.
+	// Importantly, we then return from the handler. If we don't return the handler
+	// would keep executing and also write the "Hello from SnippetBox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -16,6 +24,19 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		//If you don’t call w.WriteHeader() explicitly, then the first call to w.Write() will
+		//automatically send a 200 OK status code to the user. So, if you want to send a non-200
+		//status code, you must call w.WriteHeader() before any call to w.Write().
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 func main() {
